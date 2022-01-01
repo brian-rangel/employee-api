30a6e97c461a204b12c55b9cebefaf16fc963399
Brian Rangel <brianrang13@gmail.com>
2022-01-01 12:22:03 -0500

    Initial commit

---
+++ src/main/java/brianrangel/employeeapi/EmployeeAPIApp.java
@@ -0,0 +1,13 @@
+package brianrangel.employeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAPIApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAPIApp.class, args);
+	}
+
+}
---
+++ src/test/java/brianrangel/employeeapi/EmployeeAPIAppTests.java
@@ -0,0 +1,13 @@
+package brianrangel.employeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAPIAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
